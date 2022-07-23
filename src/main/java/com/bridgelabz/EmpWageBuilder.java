de19a5331435479989d2a177a2ae96044010bc58
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-23 22:38:31 +0530

    Added : UC5

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -9,19 +9,28 @@ public class EmpWageBuilder {
         int FULL_TIME = 8;
         int PART_TIME = 4;
 
-        double attendance = Math.floor(Math.random() * 10) % 3;
-        switch ((int) attendance) {
-            case IS_FULL_TIME -> {
-                System.out.println("Employee Wage = " + WAGE_PER_HR * FULL_TIME);
-                break;
+        int TOTAL_WORKING_DAYS = 20;
+        int wage_per_month = 0;
+        int emp_hr = 0;
+
+        for (int day = 0; day < TOTAL_WORKING_DAYS; day++) {
+            double attendance = Math.floor(Math.random() * 10) % 3;
+            switch ((int) attendance) {
+                case IS_FULL_TIME -> {
+                    emp_hr = FULL_TIME;
+                    break;
+                }
+                case IS_PART_TIME -> {
+                    emp_hr = PART_TIME;
+                    break;
+                }
+                default -> {
+                    emp_hr = 0;
+                    break;
+                }
             }
-            case IS_PART_TIME -> {
-                System.out.println("Employee Wage = " + WAGE_PER_HR * PART_TIME);
-                break;
-            }
-            default -> {
-                System.out.println("Employee Wage = " + 0);
-            }
+            wage_per_month += WAGE_PER_HR*emp_hr;
         }
+        System.out.println("Wage per month = " +wage_per_month);
     }
 }
