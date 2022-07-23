60e7d583a28ccb161ad0bc9525057f09157ce936
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-23 23:22:57 +0530

    Added : UC6

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -5,15 +5,19 @@ public class EmpWageBuilder {
         System.out.println("Welcome to Employee Wage Computation Program ");
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
-        int WAGE_PER_HR = 20;
-        int FULL_TIME = 8;
-        int PART_TIME = 4;
+        final int WAGE_PER_HR = 20;
+        final int FULL_TIME = 8;
+        final int PART_TIME = 4;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_HRS = 100;
 
-        int TOTAL_WORKING_DAYS = 20;
         int wage_per_month = 0;
+        int total_working_hrs = 0;
         int emp_hr = 0;
+        int day =0;
 
-        for (int day = 0; day < TOTAL_WORKING_DAYS; day++) {
+        while ( total_working_hrs < MAX_HRS && day < MAX_WORKING_DAYS ) {
+            day++;
             double attendance = Math.floor(Math.random() * 10) % 3;
             switch ((int) attendance) {
                 case IS_FULL_TIME -> {
@@ -29,8 +33,11 @@ public class EmpWageBuilder {
                     break;
                 }
             }
+            total_working_hrs += emp_hr;
             wage_per_month += WAGE_PER_HR*emp_hr;
         }
+        System.out.println("No of days = " +day);
+        System.out.println("No of hrs = " +total_working_hrs);
         System.out.println("Wage per month = " +wage_per_month);
     }
 }
