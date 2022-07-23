bbe70b46c2a6c619185a9b566dbb713e82494cf6
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-23 21:55:21 +0530

    Added : UC3

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -4,15 +4,18 @@ public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program ");
      int IS_FULL_TIME = 1;
+     int IS_PART_TIME = 2;
      int WAGE_PER_HR = 20;
      int FULL_TIME = 8;
-     int NO_TIME = 0;
+     int PART_TIME = 4;
 
-     int attendance = (int)Math.floor(Math.random() * 10) % 2;
+     double attendance = Math.floor(Math.random() * 10) % 3;
      if(attendance == IS_FULL_TIME) {
          System.out.println("Employee Wage = " + WAGE_PER_HR*FULL_TIME);
-     }else{
-         System.out.println("Employee Wage = " + WAGE_PER_HR*NO_TIME);
+     }else if (attendance == IS_PART_TIME){
+         System.out.println("Employee Wage = " + WAGE_PER_HR*PART_TIME);
+     } else{
+         System.out.println("Employee Wage = " + 0);
      }
     }
 }
