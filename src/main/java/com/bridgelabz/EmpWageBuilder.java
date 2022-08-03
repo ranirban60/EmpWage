d3ce367c6f4025fb24bc66c611f46a9339460fcb
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-03 22:27:25 +0530

    Added UC8

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -2,34 +2,35 @@ package com.bridgelabz;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        totalEmpWage();
+        System.out.println("Welcome to Employee Wage Computation Program ");
+        totalEmpWage("Amazon",30, 25, 200);
+        totalEmpWage("Flipkart",20, 20, 150);
         }
 
-    private static void totalEmpWage() {
-        System.out.println("Welcome to Employee Wage Computation Program ");
+    private static void totalEmpWage(String companyName, int wagePerHr, int maxWorkingDays,int maxWorkingHours) {
+
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
-        final int WAGE_PER_HR = 20;
-        final int FULL_TIME = 8;
-        final int PART_TIME = 4;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_HRS = 100;
 
-        int wage_per_month = 0;
-        int total_working_hrs = 0;
+        int totalWage = 0;
         int emp_hr = 0;
-        int day =0;
 
-        while ( total_working_hrs < MAX_HRS && day < MAX_WORKING_DAYS ) {
-            day++;
+        System.out.println("Employee of " + companyName);
+        System.out.println("Wage per hour " + wagePerHr);
+        System.out.println("Maximum working days " + maxWorkingDays);
+        System.out.println("Maximum working hours " + maxWorkingHours + "\n");
+
+
+        for (int day =1, total_working_hrs =0; total_working_hrs <= maxWorkingHours && day <= maxWorkingDays; day++, total_working_hrs += emp_hr ) {
+
             double attendance = Math.floor(Math.random() * 10) % 3;
             switch ((int) attendance) {
                 case IS_FULL_TIME -> {
-                    emp_hr = FULL_TIME;
+                    emp_hr = 8;
                     break;
                 }
                 case IS_PART_TIME -> {
-                    emp_hr = PART_TIME;
+                    emp_hr = 4;
                     break;
                 }
                 default -> {
@@ -37,12 +38,14 @@ public class EmpWageBuilder {
                     break;
                 }
             }
-            total_working_hrs += emp_hr;
-            wage_per_month += WAGE_PER_HR*emp_hr;
+            int wage = wagePerHr * emp_hr;
+            totalWage += wage;
+            System.out.println("No of days = " + day);
+            System.out.println("No of working hrs = " + emp_hr);
+            System.out.println("Wage only day " + day + " is = " +wage);
+            System.out.println("Total Wage upto day " + day + " is = " + totalWage + "\n");
         }
-        System.out.println("No of days = " +day);
-        System.out.println("No of hrs = " +total_working_hrs);
-        System.out.println("Wage per month = " +wage_per_month);
+        System.out.println("Total wage for a month of the " + companyName + " employee is " + totalWage + "\n");
     }
 }
 
