80ed9be6ecdc33340ce357dac8edc39ba0b64e9c
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-04 21:26:31 +0530

    Added: UC8

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,19 +1,19 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program ");
         totalEmpWage("Amazon",30, 25, 200);
         totalEmpWage("Flipkart",20, 20, 150);
         }
 
-    private static void totalEmpWage(String companyName, int wagePerHr, int maxWorkingDays,int maxWorkingHours) {
-
-        final int IS_FULL_TIME = 1;
-        final int IS_PART_TIME = 2;
-
-        int totalWage = 0;
+    private static int totalEmpWage(String companyName, int wagePerHr, int maxWorkingDays,int maxWorkingHours) {
         int emp_hr = 0;
+        int totalWorkingHrs = 0;
+        int totalWorkingDays = 0;
 
         System.out.println("Employee of " + companyName);
         System.out.println("Wage per hour " + wagePerHr);
@@ -21,8 +21,8 @@ public class EmpWageBuilder {
         System.out.println("Maximum working hours " + maxWorkingHours + "\n");
 
 
-        for (int day =1, total_working_hrs =0; total_working_hrs <= maxWorkingHours && day <= maxWorkingDays; day++, total_working_hrs += emp_hr ) {
-
+        while (totalWorkingHrs < maxWorkingHours && totalWorkingDays < maxWorkingDays ) {
+            totalWorkingDays++;
             double attendance = Math.floor(Math.random() * 10) % 3;
             switch ((int) attendance) {
                 case IS_FULL_TIME -> {
@@ -38,14 +38,15 @@ public class EmpWageBuilder {
                     break;
                 }
             }
-            int wage = wagePerHr * emp_hr;
-            totalWage += wage;
-            System.out.println("No of days = " + day);
-            System.out.println("No of working hrs = " + emp_hr);
-            System.out.println("Wage only day " + day + " is = " +wage);
-            System.out.println("Total Wage upto day " + day + " is = " + totalWage + "\n");
+            totalWorkingHrs += emp_hr;
+            System.out.println("No of days = " + totalWorkingDays);
+            System.out.println("No of working hrs = " + emp_hr );
         }
+        int totalWage = wagePerHr * totalWorkingHrs;
+        System.out.println();
+        System.out.println("No of Total Working Hrs in the month is " + totalWorkingHrs);
         System.out.println("Total wage for a month of the " + companyName + " employee is " + totalWage + "\n");
+        return totalWage;
     }
 }
 
