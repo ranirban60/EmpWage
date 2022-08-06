43f5a3dcb41362b372b315bf1cb77a5677f76ea5
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-06 20:47:04 +0530

    Added: UC12

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,24 +1,29 @@
 package com.bridgelabz;
-public class EmpWageBuilderArray implements EmployeeWageComputation {
+
+import java.util.ArrayList;
+
+public class EmpWageBuilder implements EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-     int numOfCompany = 0;
-     CompanyEmpWage[] companyEmpWageArray;
+     int numOfCompany = 0 ;
+     ArrayList<CompanyEmpWage> companyEmpWageList;
 
-    public EmpWageBuilderArray() {
-        companyEmpWageArray = new CompanyEmpWage[5];
+    public EmpWageBuilder() {
+        companyEmpWageList = new ArrayList<>();
     }
 
     public void addCompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHours) {
-        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHours);
+        CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHours);
+        companyEmpWageList.add(companyEmpWage);
         numOfCompany++;
     }
 
     public void computeEmpWage() {
-        for (int i = 0; i < numOfCompany; i++) {
-            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-            System.out.println(companyEmpWageArray[i]);
+        for (int i = 0; i < companyEmpWageList.size(); i++) {
+            CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+            companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+            System.out.println(companyEmpWage);
         }
     }
 
@@ -26,9 +31,8 @@ public class EmpWageBuilderArray implements EmployeeWageComputation {
         int emp_hr = 0 ;
         int totalWorkingHrs = 0;
         int totalWorkingDays = 0;
-
-        while (totalWorkingHrs <= companyEmpWage.maxWorkingHours && totalWorkingDays < companyEmpWage.maxWorkingDays) {
-            totalWorkingDays++;
+         while (totalWorkingHrs <= companyEmpWage.maxWorkingHours && totalWorkingDays < companyEmpWage.maxWorkingDays) {
+          totalWorkingDays++;
             int attendance = (int) Math.floor(Math.random() * 10) % 3;
             switch (attendance) {
                 case IS_FULL_TIME -> {
@@ -40,10 +44,10 @@ public class EmpWageBuilderArray implements EmployeeWageComputation {
                     emp_hr = 4;
                     System.out.println("Employee is working PartTime");
                     break;
-                }
+               }
                 default -> {
-                    emp_hr = 0;
-                    System.out.println("Employee is Absent");
+                   emp_hr = 0;
+                   System.out.println("Employee is Absent");
                     break;
                 }
             }
@@ -51,12 +55,12 @@ public class EmpWageBuilderArray implements EmployeeWageComputation {
             System.out.println("No of days = " + totalWorkingDays);
             System.out.println("No of working hrs = " + emp_hr);
         }
-        return totalWorkingHrs * companyEmpWage.wagePerHr;
+       return totalWorkingHrs * companyEmpWage.wagePerHr;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program ");
-        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
         empWageBuilder.addCompanyEmpWage("Amazon", 30, 25, 200);
         empWageBuilder.addCompanyEmpWage("Flipkart", 20, 20, 150);
         empWageBuilder.computeEmpWage();
