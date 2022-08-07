76b3fb3d6796d84fb95aa9843e43dc3439767ad9
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-07 11:04:04 +0530

    Added: Modified by ArrayList in UC12

--- src/main/java/com/bridgelabz/CompanyEmpWage.java
+++ src/main/java/com/bridgelabz/CompanyEmpWage.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-interface EmployeeWageComputation{
+interface IEmployeeWageComputation{
     public void addCompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHours);
     public void computeEmpWage();
 }
@@ -11,19 +11,27 @@ public class CompanyEmpWage {
     public final int maxWorkingDays;
     public final int maxWorkingHours;
     public int totalEmpWage;
-
+    //Constructor
     public CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHours) {
         this.companyName = companyName;
         this.wagePerHr = wagePerHr;
         this.maxWorkingDays = maxWorkingDays;
         this.maxWorkingHours = maxWorkingHours;
+        totalEmpWage = 0 ;
     }
 
     public void setTotalEmpWage(int totalEmpWage) {
         this.totalEmpWage = totalEmpWage;
     }
+
     @Override
     public String toString() {
+        System.out.println("                                                             ");
+        System.out.println("* Total Information of " + companyName + " employee");
+        System.out.println("Wage per hour:" + wagePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkingHours);
         return "Total wage for a month of the Company " + companyName + " is " + totalEmpWage;
     }
+
 }
--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,24 +1,25 @@
 package com.bridgelabz;
 
-import java.util.ArrayList;
+import java.util.ArrayList;// Impoprting ArrayList Class
 
-public class EmpWageBuilder implements EmployeeWageComputation {
+
+public class EmpWageBuilder implements IEmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-     int numOfCompany = 0 ;
+    //ArrayList Declaration
      ArrayList<CompanyEmpWage> companyEmpWageList;
 
+    //Constructor for EmpWageBuilder Class
     public EmpWageBuilder() {
         companyEmpWageList = new ArrayList<>();
     }
-
+    //Assigning to the Array
     public void addCompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHours) {
         CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHours);
         companyEmpWageList.add(companyEmpWage);
-        numOfCompany++;
     }
-
+    // Print Company Wage
     public void computeEmpWage() {
         for (int i = 0; i < companyEmpWageList.size(); i++) {
             CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
@@ -26,11 +27,12 @@ public class EmpWageBuilder implements EmployeeWageComputation {
             System.out.println(companyEmpWage);
         }
     }
-
+    //Computing Company Wage
      int computeEmpWage(CompanyEmpWage companyEmpWage) {
         int emp_hr = 0 ;
         int totalWorkingHrs = 0;
         int totalWorkingDays = 0;
+
          while (totalWorkingHrs <= companyEmpWage.maxWorkingHours && totalWorkingDays < companyEmpWage.maxWorkingDays) {
           totalWorkingDays++;
             int attendance = (int) Math.floor(Math.random() * 10) % 3;
@@ -50,6 +52,7 @@ public class EmpWageBuilder implements EmployeeWageComputation {
                    System.out.println("Employee is Absent");
                     break;
                 }
+
             }
             totalWorkingHrs += emp_hr;
             System.out.println("No of days = " + totalWorkingDays);
@@ -57,9 +60,11 @@ public class EmpWageBuilder implements EmployeeWageComputation {
         }
        return totalWorkingHrs * companyEmpWage.wagePerHr;
     }
-
+    //Starting of Main Method
     public static void main(String[] args) {
+        //Welcome Message
         System.out.println("Welcome to Employee Wage Computation Program ");
+        //Creating object
         EmpWageBuilder empWageBuilder = new EmpWageBuilder();
         empWageBuilder.addCompanyEmpWage("Amazon", 30, 25, 200);
         empWageBuilder.addCompanyEmpWage("Flipkart", 20, 20, 150);
